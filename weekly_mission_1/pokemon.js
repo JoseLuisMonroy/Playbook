5b42158956b58522f03715805a5785432d0cdbac
Jose Monroy <mnry7999@gmail.com>
2022-05-07 16:40:34 -0500

    Eliminar detalles

--- weekly_mission_1/pokemon.js
+++ weekly_mission_1/pokemon.js
@@ -1,13 +1,5 @@
-/*
-    Crea la clase.
-    Crea un constructor que permita recibir un nombre.
-    Crea la función sayHello().
-    Crea la función sayMessage().
-    Exporta la clase para que pueda ser importada en el archivo main.js.
-*/
 class pokemon {
     constructor(name) {
-        // Al crear este objeto se guardarán estos valores
         this.name = name
     }
     sayHello() {
