10fc356fc2c400791fe467a551461156b5b27423
Jose Monroy <mnry7999@gmail.com>
2022-05-07 16:52:24 -0500

    Creando pokemon por ESM
    Se cre aun pokemon pero empleando EcmaScript por export default

---
+++ weekly_mission_1/example9/pokemon.js
@@ -0,0 +1,8 @@
+export default class pokemon {
+    constructor(name) {
+        this.name = name
+    }
+    sayHello() {
+        console.log('Mi pokemon ' + this.name + ' te saluda!! ')
+    }
+}
