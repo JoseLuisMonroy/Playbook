c3ee7f88f73bf83fbd10b9995dfc6a22a8299e80
Jose Monroy <mnry7999@gmail.com>
2022-07-11 14:19:40 -0500

    Agregando codigo para que la prueba pase

---
+++ weekly_mission_3/spiderverse/app/spiderMan.js
@@ -0,0 +1,11 @@
+class spiderMan {
+    constructor(name, age, actor, movies, studio) {
+        this.name = name;
+        this.age = age;
+        this.actor = actor;
+        this.movies = movies;
+        this.studio = studio;
+    }
+}
+
+module.exports = spiderMan;
