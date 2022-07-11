9ce121cf1d9c09391bbc3bfda351452439d3a48c
Jose Monroy <mnry7999@gmail.com>
2022-07-11 15:05:18 -0500

    Modificando la prueba unitaria
    Se modifica la prueba para que las fechas sean asignadas en automático

--- weekly_mission_3/twitter/test/models/user.test.js
+++ weekly_mission_3/twitter/test/models/user.test.js
@@ -2,12 +2,12 @@ const user = require('../../app/models/user');
 
 describe("Pruebas unitarias para User", () => {
     test('Caso 1 Crear un objeto de user', () => {
-        const nuevUser = new user("1", "J0se", "Jose", "Random", "14/02/2020", "15/02/2020");
+        const nuevUser = new user("1", "J0se", "Jose", "Random");
         expect(nuevUser.id).toBe("1");
         expect(nuevUser.username).toBe("J0se");
         expect(nuevUser.name).toBe("Jose");
         expect(nuevUser.bio).toBe("Random");
-        expect(nuevUser.dateCreated).toBe("14/02/2020");
-        expect(nuevUser.lastUpdated).toBe("15/02/2020");
+        expect(nuevUser.dateCreated).not.toBeUndefined();
+        expect(nuevUser.lastUpdated).not.toBeUndefined();
     });
 });
