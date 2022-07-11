0a752b7f0faad159a83ffc74177296fe4f22b33d
Jose Monroy <mnry7999@gmail.com>
2022-07-11 13:35:33 -0500

    Agregando prueba que pasa

--- weekly_mission_3/my_launchx_app/test/missionCommander.test.js
+++ weekly_mission_3/my_launchx_app/test/missionCommander.test.js
@@ -3,6 +3,6 @@ const MissionCommander = require('./../app/missionCommander');
 describe("Prueba unitaria para la clase de missionCommander", () => {
     test('1) Crear un objeto tipo mission commander', () => {
         const myMissionCommander = new MissionCommander("Woopa")
-        expect(myMissionCommander.name).toBe("Wooopa");
+        expect(myMissionCommander.name).toBe("Woopa");
     });
 })
