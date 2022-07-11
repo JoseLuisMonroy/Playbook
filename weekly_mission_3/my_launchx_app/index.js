517054f8cff87dde741b02b7e4ef135c3beece9e
Jose Monroy <mnry7999@gmail.com>
2022-07-11 13:13:32 -0500

    Instanciando 3 objetos

---
+++ weekly_mission_3/my_launchx_app/index.js
@@ -0,0 +1,8 @@
+const MissionCommander = require('./app/missionCommander');
+
+const carlo = new MissionCommander("Carlo")
+console.log(carlo.name)
+const jose = new MissionCommander("Jose")
+console.log(jose.name)
+const rodrigo = new MissionCommander("Rodrigo")
+console.log(rodrigo.name)
