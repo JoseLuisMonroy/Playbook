9b65b32cca8de48c6fd4e660a0f4cbc29014ad9c
Jose Monroy <mnry7999@gmail.com>
2022-07-11 14:25:14 -0500

    Agregando prueba que truena

--- weekly_mission_3/spiderverse/test/spiderman.test.js
+++ weekly_mission_3/spiderverse/test/spiderman.test.js
@@ -1,12 +1,18 @@
 const spiderMan = require('./../app/spiderman.js')
 
 describe("Pruebas Unitarias para Spiderman", () => {
-    test('Case 1 Crear un objeto de spiderman', () => {
+    test('Caso 1 Crear un objeto de spiderman', () => {
         const andrew = new spiderMan("SpiderMan Sony", "30", "Andrew Garfield", "2", "Sony");
+
         expect(andrew.name).toBe("SpiderMan Sony");
         expect(andrew.age).toBe("30");
         expect(andrew.actor).toBe("Andrew Garfield");
         expect(andrew.movies).toBe("2");
         expect(andrew.studio).toBe("Sony");
+    });
+    test('Caso 2 usando metodo gethInfo', () => {
+        const tom = new spiderMan("SpiderMan Marvel", "25", "Tom Holland", "5", "Marvel");
+
+        expect(tom.getInfo()).toBe("Hey I'm Tom Holland from Marvel studio");
     });
 })
