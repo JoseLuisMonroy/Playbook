a8a2b21c092ae4830fe8e24a25aa1cc9f70877a1
Jose Monroy <mnry7999@gmail.com>
2022-07-12 12:51:48 -0500

    Agregando prueba para user test
    Esta clase deberá tener un método  que se pueda usar sin instanciar, y que reciba solo los parámetros del uid=197609(monro) gid=197609 groups=197609,  y

--- weekly_mission_3/twitter/test/models/user.test.js
+++ weekly_mission_3/twitter/test/models/user.test.js
@@ -28,3 +28,14 @@ describe("Pruebas unitarias para User", () => {
         expect(test3.bio).toBe("IDK");
     });
 });
+
+describe("Pruebas unitarias para User service", () => {
+    test('Caso 1 Crear un objeto de useCrear un nuevo usuario usando user service', () => {
+        const test4 = new userService("1", "Monroy", "Jose");
+
+        expect(test4.id).toBe("1");
+        expect(test4.username).toBe("Monroy");
+        expect(test4.name).toBe("Jose");
+        expect(test4.bio).not.toBeUndefined();
+    });
+});
