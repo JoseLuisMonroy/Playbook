aafbded343df2495bd2fac4e0c134de3ddd40c51
Jose Monroy <mnry7999@gmail.com>
2022-07-12 13:12:22 -0500

    Agregando metodo estatico

--- weekly_mission_3/twitter/app/services/userService.js
+++ weekly_mission_3/twitter/app/services/userService.js
@@ -4,6 +4,9 @@ class userService {
     static create(id, username, name) {
         return new user(id, username, name, "Sin bio");
     }
+    static getInfo(user) {
+        return [user.id, user.username, user.name, user.bio];
+    }
 }
 
 module.exports = userService;
