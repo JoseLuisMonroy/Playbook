62ef9bd5d58a8eb355695f9bc4206087d377ec2f
Jose Monroy <mnry7999@gmail.com>
2022-07-12 13:10:24 -0500

    Agregando prueba de lista
    El metodo debe regresar los datos de un usuario en una lista

--- weekly_mission_3/twitter/test/services/userService.test.js
+++ weekly_mission_3/twitter/test/services/userService.test.js
@@ -9,4 +9,13 @@ describe("Pruebas unitarias para User service", () => {
         expect(test4.name).toBe("Jose");
         expect(test4.bio).not.toBeUndefined();
     });
+    test('Caso 2 Obtener todos los datos del usuario en una lista', () => {
+        const test5 = userService.create("1", "Monroy", "Jose");
+        const test5Lista = userService.getInfo(test5);
+
+        expect(test5Lista[0]).toBe("1");
+        expect(test5Lista[1]).toBe("Monroy");
+        expect(test5Lista[2]).toBe("Jose");
+        expect(test5Lista[3]).toBe("Sin bio");
+    });
 });
