c59ac1a0d75c867098592c61445e0acc37a9aa3f
Jose Monroy <mnry7999@gmail.com>
2022-07-12 14:39:46 -0500

    Creando endpoint usando GET

--- weekly_mission_3/express_api/app.js
+++ weekly_mission_3/express_api/app.js
@@ -1,10 +1,18 @@
 const express = require('express')
 
 const app = express()
-app.use(express.json()) 
+app.use(express.json())
 
 const port = 3000
 
 app.listen(port, () => {
- console.log(`Example app listening on port ${port}`)
+    console.log(`Example app listening on port ${port}`)
 })
+
+app.get('/v1/explorers', (req, res) => {
+    const explorer1 = { name: "Jose", age: "19" }
+    const explorer2 = { name: "Juan", age: "20" }
+    const explorer3 = { name: "Pedro", age: "21" }
+    const explorers = [explorer1, explorer2, explorer3]
+    res.status(200).json(explorers)
+});
