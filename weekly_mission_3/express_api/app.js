94bfe695905d3615e2fb3951c614af2833ca1556
Jose Monroy <mnry7999@gmail.com>
2022-07-12 14:56:14 -0500

    Regresar un explorer mediante una ID en URL

--- weekly_mission_3/express_api/app.js
+++ weekly_mission_3/express_api/app.js
@@ -16,3 +16,13 @@ app.get('/v1/explorers', (req, res) => {
     const explorers = [explorer1, explorer2, explorer3]
     res.status(200).json(explorers)
 });
+
+app.get('/v1/explorers/:id', (req, res) => {
+    console.log('API Explorers GET request ${new Date()}')
+    console.log('Getting explorer with id: ${req.params.id}')
+    const explorer = { id: 1, name: "Jose", age: "19" }
+    const explorer1 = { id: 2, name: "Jose", age: "19" }
+    const explorer2 = { id: 3, name: "Juan", age: "20" }
+    const explorers = [explorer, explorer1, explorer2]
+    res.status(200).json(explorers[req.params.id - 1])
+})
