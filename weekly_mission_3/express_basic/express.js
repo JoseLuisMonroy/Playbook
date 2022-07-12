e1f97e63e425287489bda5fcd736ccb61a7c22a2
Jose Monroy <mnry7999@gmail.com>
2022-07-12 14:27:37 -0500

    Agregando rutas y regreso de objetos tipo JSON

--- weekly_mission_3/express_basic/express.js
+++ weekly_mission_3/express_basic/express.js
@@ -9,3 +9,12 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}!`)
 });
+
+app.get('/launchx', (req, res) => {
+    res.send('Woopa!')
+});
+
+app.get('/explorers', (req, res) => {
+    const explorer = { name: "Jose", age: "19" }
+    res.send(explorer)
+});
