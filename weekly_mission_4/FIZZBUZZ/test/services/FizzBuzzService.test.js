efcdc0f8d5e4f4ee6a96a69cfc7fffdee496a783
Jose Monroy <mnry7999@gmail.com>
2022-07-13 14:30:23 -0500

    Corrigiendo los parametros de la prueba

--- weekly_mission_4/FIZZBUZZ/test/services/FizzBuzzService.test.js
+++ weekly_mission_4/FIZZBUZZ/test/services/FizzBuzzService.test.js
@@ -1,15 +1,24 @@
+const FizzbuzzService = require('../../lib/services/FizzBuzzService');
+
 describe('FizzBuzzService test', () => {
-    TextDecoderStream('Caso 1 Validación Fizzbuzz', () => {
+    test('Caso 1 Cuando nop hay module 3 y 5', () => {
         const explorer1 = { name: "Explorer1", score: 1 }
-        FizzbuzzService.applyValidationInExplorer(explorer1) // {name: "Explorer1", score: 1, trick: 1} 
-
+        const result1 = FizzbuzzService.applyValidationInExplorer(explorer1)
+        expect(result1.trick).toBe(1)
+    });
+    test('Cas0 2 Cuando hay modulo 3', () => {
         const explorer3 = { name: "Explorer3", score: 3 }
-        FizzbuzzService.applyValidationInExplorer(explorer3) // {name: "Explorer3", score: 3, trick: "FIZZ"}
-
+        const resultado2 = FizzbuzzService.applyValidationInExplorer(explorer3)
+        expect(resultado2.trick).toBe("FIZZ")
+    });
+    test('Caso 3 Cuando hay modulo 5', () => {
         const explorer5 = { name: "Explorer5", score: 5 }
-        FizzbuzzService.applyValidationInExplorer(explorer5) // {name: "Explorer5", score: 5, trick: "BUZZ"}
-
+        const resultado3 = FizzbuzzService.applyValidationInExplorer(explorer5)
+        expect(resultado3.trick).toBe("BUZZ")
+    });
+    test('Caso 4 Cuando hay modulo 3 y 5', () => {
         const explorer15 = { name: "Explorer15", score: 15 }
-        FizzbuzzService.applyValidationInExplorer(explorer15) // {name: "Explorer15", score: 15, trick: "FIZZBUZZ"}
+        const resultado4 = FizzbuzzService.applyValidationInExplorer(explorer15)
+        expect(resultado4.trick).toBe("FIZZBUZZ")
     });
 });
