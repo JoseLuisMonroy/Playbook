84ba86d508c642336b590fa3959848eb15a21988
Jose Monroy <mnry7999@gmail.com>
2022-07-13 16:21:53 -0500

    Pruebas unitarias para el reader

---
+++ weekly_mission_4/FIZZBUZZ/test/utils/Reader.test.js
@@ -0,0 +1,12 @@
+const fs = require("fs");
+const reader = require("./../../lib/utils/Reader");
+
+describe("Reader test suite", () => {
+    test('Caso 1 Leer el archivo JSON', () => {
+        const rawdata = fs.readFileSync("explorers.json");
+        const explorers = JSON.parse(rawdata);
+
+        expect(explorers).toBeDefined();
+        expect(explorers.length).toBe(15);
+    });
+});
